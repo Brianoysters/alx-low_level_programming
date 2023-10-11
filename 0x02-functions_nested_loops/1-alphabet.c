1241442e8d8335ec88599235b0b50ccaf9aa78a2
Brianoysters <brianoysters@gmail.com>
2023-10-11 01:16:43 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - outputs the alphabet, in lowercase, followed by a new line.
+ * Return : 0 (success)
+ */
+
+void print_alphabet(void)
+{
+
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
