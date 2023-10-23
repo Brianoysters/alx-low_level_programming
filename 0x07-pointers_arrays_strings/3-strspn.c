b8eef466c662c87e59aaec31b375123ef3d1b8e4
Brianoysters <brianoysters@gmail.com>
2023-10-23 05:11:44 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,24 +8,23 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int i = 0;
+	int p;
 
-        unsigned int _strspn(char *s, char *accept);
-        unsigned int i = 0;
-        int p;
-
-        while (*s)
-        {
-        for (p = 0; accept[p]; p++)
-        {
-        if (*s == accept[p])
-        {
-        i++;
-        break;
-        }
-        else if (accept[p + 1] == '\0')
-        return (i);
-        }
-        s++;
-        }
-        return (i);
+	while (*s)
+	{
+		for (p = 0; accept[p]; p++)
+		{
+			if (*s == accept[p])
+			{
+				i++;
+				break;
+			}
+			else if (accept[p + 1] == '\0')
+				return (i);
+		}
+		s++;
+	}
+	return (i);
 }
