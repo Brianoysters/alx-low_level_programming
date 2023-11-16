1065f4ae443d63393a58b55843ac0564ef55cfef
Brianoysters <brianoysters@gmail.com>
2023-11-16 09:37:34 -0800

    more updates

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,18 +2,18 @@
 
 /**
  * This line defines a function called first with the
- * __attribute__((constructor)) directive. 
+ * __attribute__((constructor)) directive.
  * This directive indicates that the first function will be
  * executed before the main function is called.
  */
-void first()__attribute__((constructor));
+void first(void)__attribute__((constructor));
 
 /**
  * first - outputs the message
  */
 
-void first()
+void first(void)
 {
-  printf("You're beat! and yet, you must allow,\n");
-  printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
